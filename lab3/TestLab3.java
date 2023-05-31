633401c94d3f07dfb0bf7b5c5dcfc56ddb58c60a
matthewghuang <matthewhhuang@gmail.com>
2023-05-31 12:18:31 -0700

    add javadocs

--- lab3/TestLab3.java
+++ lab3/TestLab3.java
@@ -42,78 +42,152 @@ public class TestLab3{
 }
 enum Status{TEMPORARY,PART_TIME, FULL_TIME};
 
+/**
+	Represents a person
+ */
 class Person{
 	private String name;
 	private String email;
 	private String phoneNumber;	
 
+	/**
+		Default constructor
+	 */
 	public Person() {
 	}
 
+	/**
+		Creates a person
+		@param name The person's name.
+		@param email The person's email.
+		@param phoneNumber The person's phone number.
+	 */
 	public Person(String name, String email, String phoneNumber) {
 		this.name = name;
 		this.email = email;
 		this.phoneNumber = phoneNumber;
 	}
 
+	/**
+		Modify a person's info
+		@param name The new name.
+		@param email The new email.
+		@param phoneNumber The new phone number.
+	 */
 	public void modifyInfo(String name, String email, String phoneNumber) {
 		this.name = name;
 		this.email = email;
 		this.phoneNumber = phoneNumber;
 	}
 
+	/**
+		Get the name.
+		@return The person's name.
+	 */
 	public String getName() {
 		return this.name;
 	}
 
+	/**
+		Get the email.
+		@return The person's email.
+	 */
 	public String getEmail() {
 		return this.email;
 	}
 
+	/**
+		Get the phone number.
+		@return The person's phone number.
+	 */
 	public String getPhoneNumber() {
 		return this.phoneNumber;
 	}
 
+	/**
+		Represent the person as a String.
+		@return Person in a String in the format "name: %s, email: %s, phone number: %s".
+	 */
 	@Override
 	public String toString() {
 		return String.format("name: %s, email: %s, phone number: %s", name, email, phoneNumber);
 	}
 }
 
+/**
+	Represents an employee with a default office of A101 and a salary of 40000.
+ */
 class Employee extends Person{
 	private String office = "A101";
 	private double salary = 40000;	
 
+	/**
+		Empty constructor.
+	 */
 	public Employee() {
 		super();
 	}
 
+	/**
+		Creates an employee.
+		@param name The employee's name.
+		@param email The employee's email.
+		@param phoneNumber The employee's phone number.
+	 */
 	public Employee(String name, String email, String phoneNumber) {
 		super(name, email, phoneNumber);
 	}
 
+	/**
+		Creates an employee.
+		@param name The employee's name.
+		@param email The employee's email.
+		@param phoneNumber The employee's phone number.
+		@param office The employee's office.
+		@param salary The employee's salary.
+	 */
 	public Employee(String name, String email, String phoneNumber, String office, double salary) {
 		this(name, email, phoneNumber);
 		this.office = office;
 		this.salary = salary;
 	}
 
+	/**
+		Set the employee's office.
+		@param office The new office.	
+	 */
 	public void setOffice(String office) {
 		this.office = office;
 	}
 
+	/**
+		Set the employee's salary.
+		@param salary The new salary.
+	 */
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
 
+	/**
+		Get the employee's salary.
+		@return The salary.	
+	 */
 	public double getSalary() {
 		return salary;
 	}
 
+	/**
+		Get the employee's office.
+		@return The office.	
+	 */
 	public String getOffice() {
 		return office;
 	}
 
+	/**
+		Represent the employee as a String.
+		@return Employee in a String in the format "name: %s, email: %s, phone number: %s, office: %s, salary: %.2f".
+	 */
 	@Override
 	public String toString() {
 		return String.format("%s, office: %s, salary: %.2f", super.toString(), office, salary);
@@ -124,14 +198,34 @@ class Employee extends Person{
 class Faculty extends Employee{
 	private Status status = Status.TEMPORARY;
 
+	/**
+     * Constructs a new Faculty object with default values.
+     */
 	public Faculty() {
 		super();
 	}
 
+	/**
+     * Constructs a new Faculty object with the specified name, email, and phone number.
+     *
+     * @param name         the name of the faculty member
+     * @param email        the email address of the faculty member
+     * @param phoneNumber  the phone number of the faculty member
+     */
 	public Faculty(String name, String email, String phoneNumber) {
 		super(name, email, phoneNumber);
 	}
 
+	/**
+     * Constructs a new Faculty object with the specified name, email, phone number, office, salary, and status.
+     *
+     * @param name         the name of the faculty member
+     * @param email        the email address of the faculty member
+     * @param phoneNumber  the phone number of the faculty member
+     * @param office       the office location of the faculty member
+     * @param salary       the salary of the faculty member
+     * @param status       the employment status of the faculty member
+     */
 	public Faculty(String name, String email, String phoneNumber, String office, double salary, Status status) {
 		super(name, email, phoneNumber, office, salary);
 		this.status = status;
